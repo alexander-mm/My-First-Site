5f97029f58187947bd2a8b0b85450e29207f8edc
codeengineersgroup <alexanderwolftrader@gmail.com>
2022-05-03 19:48:20 -0500

    ENTREGA-FINAL

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ const btnSwitch = document.querySelector("#onoff");
 btnSwitch.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     btnSwitch.classList.toggle("active");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark-mode", "true");
+    }else{
+        localStorage.setItem("dark-mode", "false");
+    }
 });
+
+if(localStorage.getItem("dark-mode") === "true"){
+    document.body.classList.add("dark");
+    btnSwitch.classList.add("active");
+}   else{
+    document.body.classList.remove("dark");
+    btnSwitch.classList.remove("active");
+}
